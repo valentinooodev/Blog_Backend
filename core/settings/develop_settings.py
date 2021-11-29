606a4a673fbbe16fb2e71bfecc09f506a5dd57fc
= <=>
2021-11-29 19:06:10 +0700

    init django

---
+++ core/settings/develop_settings.py
@@ -0,0 +1,13 @@
+from core.default_settings import *
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
