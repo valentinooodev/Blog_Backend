1b86be1d1c993bb99a0738d170ea69121bff24d4
= <=>
2021-12-01 18:19:28 +0700

    core api & open api schemas

--- core/default_settings.py
+++ core/default_settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 # CORS_ALLOWED_ORIGINS = [
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,11 @@ urlpatterns = [
     path('api/v1/user/', include('users.urls', namespace='users')),
     path('api/v1/', include('blog_api.urls', namespace='blog_api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('docs/', include_docs_urls(title='PythonistaAPI')),
+    path('schema/', get_schema_view(
+        title='Pythonista Blog',
+        description='API for Pythonista Blog',
+        version='1.0.1'
+    ),  name='openapi-schema'),
 ]
+
